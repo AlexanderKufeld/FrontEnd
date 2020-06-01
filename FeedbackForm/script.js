59acd9882c980e27a5b97867c4a3d8ad7a71edfe
AlexanderKufeld <alexander.kufeld@gmail.com>
2020-06-01 15:27:27 +0200

    Feedback_Form

--- FeedbackForm/script.js
+++ FeedbackForm/script.js
@@ -15,8 +15,9 @@ document.getElementById('count').innerHTML = "Total number of records: " + perso
 let persons = [];
 
 const form$ = document.getElementsByTagName('form')[0];
-form$.addEventListener('submit', function (event) {
+    form$.addEventListener('submit', function (event) {
     event.preventDefault();
+
     let person = {};
     persons.push(person);
     document.querySelector('input[name=name]').value = '';
@@ -25,14 +26,19 @@ form$.addEventListener('submit', function (event) {
     document.querySelector("input[name=Number of Record]").value='';
     });
 
-    const name = document.getElementById('name');
-    const eMail = document.getElementById('eMail');
-    const Feedback = document.getElementById('Feedback');
+        const name = document.getElementById('name');
+        const eMail = document.getElementById('eMail');
+        const Feedback = document.getElementById('Feedback');
+        const NumberOfRecord = document.getElementById('Number of Record');
+        const Date = document.getElementById('date');
+
+
 
     let clicks= 0;
     function click() {
     clicks += 1;
     document.getElementById("Number of Record").innerHTML = clicks;
     };
-   
+
 showUsersTable(persons);
+document.write(Date());
